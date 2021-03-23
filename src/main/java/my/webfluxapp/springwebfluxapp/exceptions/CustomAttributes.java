5b4fab4576a7e22ecb4d1c981ae9844d11355ba3
Insane <valeev.timur.v@gmail.com>
2021-03-23 22:07:38 +0300

    Create custom Attributes to DefaultErrorAttributes

---
+++ src/main/java/my/webfluxapp/springwebfluxapp/exceptions/CustomAttributes.java
@@ -0,0 +1,37 @@
+package my.webfluxapp.springwebfluxapp.exceptions;
+
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+/**
+ * @author
+ * @return
+ */
+
+@Component
+public class CustomAttributes extends DefaultErrorAttributes {
+
+
+    @Override
+    public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributesMap = super.getErrorAttributes(request, options);
+        Throwable throwable = getError(request);
+        if (throwable instanceof ResponseStatusException){
+            ResponseStatusException ex= (ResponseStatusException) throwable;
+            errorAttributesMap.put("message", ex.getMessage());
+            errorAttributesMap.put("developerMessage", "A responseStatusException Happened");
+            return errorAttributesMap;
+        }
+        return errorAttributesMap;
+    }
+
+
+}
