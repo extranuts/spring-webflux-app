7fb21d76966bbcc7a03ee9d4970ed0c269beaa49
Insane <valeev.timur.v@gmail.com>
2021-03-24 15:51:26 +0300

    Add logback-test.xml and logic test level info

--- src/test/java/my/webfluxapp/springwebfluxapp/SpringWebfluxAppApplicationTests.java
+++ src/test/java/my/webfluxapp/springwebfluxapp/SpringWebfluxAppApplicationTests.java
@@ -1,10 +1,13 @@
 package my.webfluxapp.springwebfluxapp;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class SpringWebfluxAppApplicationTests {
+
 
 	@Test
 	void contextLoads() {
