07bd826b7a0bd3f63971a0339154d1913979e39d
Insane <valeev.timur.v@gmail.com>
2021-03-24 12:40:12 +0300

    Test Service If use Test JDK13+ add flag -XX:+AllowRedefinitionToAddDeleteMethods

--- src/test/java/my/webfluxapp/springwebfluxapp/service/AnimeServiceTest.java
+++ src/test/java/my/webfluxapp/springwebfluxapp/service/AnimeServiceTest.java
@@ -3,21 +3,23 @@ package my.webfluxapp.springwebfluxapp.service;
 import my.webfluxapp.springwebfluxapp.domain.Anime;
 import my.webfluxapp.springwebfluxapp.repository.AnimeRepository;
 import my.webfluxapp.springwebfluxapp.util.AnimeCreator;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.blockhound.BlockHound;
 import reactor.blockhound.BlockingOperationError;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(SpringExtension.class)
@@ -32,12 +34,12 @@ class AnimeServiceTest {
     private final Anime anime = AnimeCreator.createValidAnime();
 
     @BeforeAll
-    public static void blockHoundSetup(){
+    public static void blockHoundSetup() {
         BlockHound.install();
     }
 
     @Test
-    public void blockHoundWorks(){
+    public void blockHoundWorks() {
         try {
             FutureTask<?> task = new FutureTask<>(() -> {
                 Thread.sleep(0);
@@ -47,11 +49,47 @@ class AnimeServiceTest {
 
             task.get(10, TimeUnit.SECONDS);
             Assertions.fail("should fail");
-        }catch (Exception e){
+        } catch (Exception e) {
             Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
         }
     }
 
+    @BeforeEach
+    public void setUp() {
+        BDDMockito.when(animeRepositoryMock.findAll())
+                .thenReturn(Flux.just(anime));
+        BDDMockito.when(animeRepositoryMock.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Mono.just(anime));
+    }
+
+    @Test
+    @DisplayName("listAll returns a flux of anime")
+    public void listAll_ReturnFluxAnime_WhenSuccessful() {
+        StepVerifier.create(animeService.findAll())
+                .expectNext(anime)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("listAll returns a Mono with anime if exists")
+    public void findById_ReturnMonoAnime_WhenSuccessful() {
+        StepVerifier.create(animeService.findByID(1))
+                .expectSubscription()
+                .expectNext(anime)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("listAll returns a Mono error when anime is not exist")
+    public void findById_ReturnMonoError_WhenEmptyIsReturned() {
+        BDDMockito.when(animeRepositoryMock.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Mono.just(anime));
+
+        StepVerifier.create(animeService.findByID(1))
+                .expectSubscription()
+                .expectError(ResponseStatusException.class)
+                .verify();
+    }
 
 
 }
